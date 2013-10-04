bfbd76000450b4fc43ad40474206224c40ab07dc
MARRECO <MARRECO@192.168.1.100>
2013-10-04 16:53:25 -0300

    Comit Inicial

---
+++ Hotel/src/br/com/hotel/Teste.java
@@ -0,0 +1,15 @@
+/*
+ * Direitos reservados para Anderson Orione Aristides
+ * www.makersweb.com.br.
+ */
+
+package br.com.hotel;
+
+/**
+ *
+ * @author Anderson Orione Aristides
+ * @version 1.0
+ */
+public class Teste {
+
+}
